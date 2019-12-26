0b6b8c5c4150c70c1f4278883d0257fadfaff72e
mtanneau <mathieu.tanneau@gmail.com>
2019-12-26 14:55:45 -0500

    update snoop script

--- compiled/snoop.jl
+++ compiled/snoop.jl
@@ -1,8 +1,8 @@
-const INST_DIR = joinpath(@__DIR__, "../dat/dummy")
+const INST_DIR = @__DIR__
 
 include("tulip_cl.jl")
 
 # Read and solve all dummy instances
 for finst in ["lpex_inf.mps", "lpex_opt.mps", "lpex_ubd.mps"]
-    Tulip_cl.julia_main(["--Threads", "1", "--TimeLimit", "1.0", joinpath(INST_DIR, finst)])
+    Tulip_cl.julia_main(["--Threads", "1", "--TimeLimit", "10.0", joinpath(INST_DIR, finst)])
 end
